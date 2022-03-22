c5d38fa74291cec60a8305c4ac727b4dfcb07d56
Jamalia - OfisLaptop <jamalia.sirun@gmail.com>
2022-03-22 10:09:26 +0800

    init

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function cubaan()
+    {
+        return view('test');
+    }
+
+    public function welcome()
+    {
+        return view('welcome');
+    }
+
+}
---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>My List</h1>
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Tarikh eHearing</th>
+                                    <th>Hari</th>
+                                    <th>Status</th>
+                                    <th>Tindakan</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>21.03.2022</td>
+                                    <td>Isnin</td>
+                                    <td>Selesai</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+
+                                <tr>
+                                    <td>22.03.2022</td>
+                                    <td>Selasa</td>
+                                    <td>Pending</td>
+                                    <td><button class="btn btn-primary">Done</button></td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+        </div>
+
+        <div class="container mt-5">
+            <div class="row text-white">
+
+                  <div class="col bg-primary text-white">Primary</div>
+                  <div class="col bg-success text-white">Joe</div>
+                  <div class="col bg-warning text-white">joe@example.com</div>
+
+
+            </div>
+          </div>
+
+
+
+
+
+</body>
+</html>
